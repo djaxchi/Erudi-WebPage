5295eb65efd2b68f7da0c36080ab7b1bee168da4
Djalil Chikhi <djalilchiki@gmail.com>
2025-06-29 00:02:40 +0200

    fixes

--- src/assets/animatedSection.tsx
+++ src/assets/animatedSection.tsx
@@ -1,11 +1,11 @@
 import { useIntersectionObserver } from './useIntersectionObserver';    
 
 // Animated reveal
-export const AnimatedSection = ({ children, delay = 0 }) => {
+export const AnimatedSection = ({ children, delay = 0 }: { children: React.ReactNode; delay?: number }) => {
   const [ref, visible] = useIntersectionObserver();
   return (
     <div
-      ref={ref}
+      ref={typeof ref === 'boolean' ? undefined : ref}
       className={`transform transition-all duration-700 ease-out ${
         visible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-6'
       }`}
