d0cb2713d0b69a2763afb77bca8fc2d24e1e0a2c
Djalil Chikhi <djalilchiki@gmail.com>
2025-06-28 23:44:47 +0200

    finishing commit for erudi project

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,52 @@
+// App.tsx
 import React from 'react';
-//import Navbar from './components/Navbar';
-//import LandingPage from './pages/LandingPage';
-import QPage from './pages/QPage';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import LandingPage from './pages/LandingPage';
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
 
-const App: React.FC = () => {
-  return (
-    <>
-      <QPage />
-    </> 
-  );
-};
+const App: React.FC = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route
+      path="/"
+      element={
+        <>
+        <Navbar activePage="/download" />
+        <LandingPage />
+        </>
+      }
+      />
+      <Route
+      path="/download"
+      element={
+        <>
+        <Navbar activePage="/download" />
+        <LandingPage />
+        </>
+      }
+      />
+      <Route
+      path="/about"
+      element={
+        <>
+        <Navbar activePage="/about" />
+        <AboutPage />
+        </>
+      }
+      />
+      <Route
+      path="/contact"
+      element={
+        <>
+        <Navbar activePage="/contact" />
+        <ContactPage />
+        </>
+      }
+      />
+    </Routes>
+  </BrowserRouter>
+);
 
 export default App;
